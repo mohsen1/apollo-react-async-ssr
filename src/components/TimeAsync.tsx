892f63c74733bcec6eb13d44e316dc7c4dee7495
Mohsen Azimi <mohsen.azimi@airbnb.com>
2019-06-23 15:40:07 -0400

    typo

--- src/components/TimeAsync.tsx
+++ src/components/TimeAsync.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import Loadable from "react-loadable";
 
 export default Loadable({
-  loader: () => import(/* webpackChunkName: "Help" */ "./Time"),
+  loader: () => import(/* webpackChunkName: "Time" */ "./Time"),
   loading: () => <p>Loading time...</p>
 });
